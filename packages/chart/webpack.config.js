cfc43fc3412a73c73e440783aedd03050c69c3af
Kuba Korecki <kubakorecki@gmail.com>
2022-09-12 19:52:02 +0200

    add svg inline loader

--- packages/chart/webpack.config.js
+++ packages/chart/webpack.config.js
@@ -2,6 +2,14 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/Chart.js",
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      }
+    ],
+  },
   output: {
     filename: "chart.js",
     path: path.resolve(__dirname, "dist"),
