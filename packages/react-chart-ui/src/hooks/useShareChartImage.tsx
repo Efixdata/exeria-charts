6af0af1fc74fb4c913762bd549ad130c54416967
Kuji Bolkvadze <58769030+kuji2336@users.noreply.github.com>
2022-12-29 13:25:38 +0400

    added dexer watermark on shared images

--- packages/react-chart-ui/src/hooks/useShareChartImage.tsx
+++ packages/react-chart-ui/src/hooks/useShareChartImage.tsx
@@ -1,24 +1,40 @@
 import { useEffect, useState } from "react";
+import Mark from "../img/icons/mark.svg";
 
 export default function useShareChartImage(chart: any) {
   const TWITTER_INTENT_URI = "https://twitter.com/intent/tweet";
   const TELEGRAM_INTENT_URI = "https://t.me/share/url";
   const API_URI = "https://dexer-images.netlify.app/.netlify/functions/api";
   const TEMPLATE_TEXT = `Check chart now 🚀🚀🚀🚀`;
-  const STARTING_POINT = window.location.href
-
+  const STARTING_POINT = window.location.href;
   const [actionLoading, setActionLoading] = useState({
     twitter: false,
     telegram: false,
-    copyImage:false
+    copyImage: false,
   });
 
-  const startSession = async () => {
-    const canvasToBase64 = chart?.chart?.canvas?.toDataURL("image/png", 1.0);
+  const createWaterMark = async () => {
+    return new Promise((resolve, reject) => {
+      const ctx = chart.chart.ctx;
+      const positionY = chart.chart?.canvasHeight / 2;
+      const positionX = chart.chart?.canvasWidth / 3;
+      const image = new Image();
+      image.onload = setUpWaterMark; // Draw when image has loaded
+      image.src = Mark.src;
+      function setUpWaterMark() {
+        ctx?.drawImage(image, positionX, positionY, 260, 80);
+        return resolve(
+          chart?.chart?.canvas?.toDataURL("image/png", 1.0) as string
+        );
+      }
+    });
+  };
+
+  const startSession = async (imageData: string | unknown) => {
     const response = await fetch(`${API_URI}/session/start`, {
       method: "POST",
       body: JSON.stringify({
-        binary: canvasToBase64,
+        binary: imageData,
       }),
     });
     return await response.json();
@@ -27,15 +43,18 @@ export default function useShareChartImage(chart: any) {
   const shareOnTwitter = async () => {
     setActionLoading((prev) => ({ ...prev, twitter: true }));
     try {
-      const callback = await startSession();
-      const redirectURI = callback.redirect_url;
-      const twitterIntentURI = `${TWITTER_INTENT_URI}?text=${TEMPLATE_TEXT}&url=${redirectURI}?t=${Date.now()}?point=${STARTING_POINT}`;
+      const imageData = await createWaterMark();
+      if (imageData) {
+        const callback = await startSession(imageData);
+        const redirectURI = callback.redirect_url;
+        const twitterIntentURI = `${TWITTER_INTENT_URI}?text=${TEMPLATE_TEXT}&url=${redirectURI}?t=${Date.now()}?point=${STARTING_POINT}`;
 
-      //open new window in SAFARI browser
-      if (redirectURI) {
-        setTimeout(() => {
-          window.open(twitterIntentURI, "_blank");
-        }, 0);
+        //open new window in SAFARI browser
+        if (redirectURI) {
+          setTimeout(() => {
+            window.open(twitterIntentURI, "_blank");
+          }, 0);
+        }
       }
     } catch (err) {
       console.error("error", err);
@@ -47,13 +66,16 @@ export default function useShareChartImage(chart: any) {
   const shareOnTelegram = async () => {
     setActionLoading((prev) => ({ ...prev, telegram: true }));
     try {
-      const callback = await startSession();
-      const redirectURI = callback.redirect_url;
-      if (redirectURI) {
-        const href = `${TELEGRAM_INTENT_URI}?url=${redirectURI}?t=${Date.now()}?point=${STARTING_POINT}&text=${TEMPLATE_TEXT}`;
-        setTimeout(() => {
-          window.open(href, "_blank");
-        }, 0);
+      const imageData = await createWaterMark();
+      if (imageData) {
+        const callback = await startSession(imageData);
+        const redirectURI = callback.redirect_url;
+        if (redirectURI) {
+          const href = `${TELEGRAM_INTENT_URI}?url=${redirectURI}?t=${Date.now()}?point=${STARTING_POINT}&text=${TEMPLATE_TEXT}`;
+          setTimeout(() => {
+            window.open(href, "_blank");
+          }, 0);
+        }
       }
     } catch (err) {
       console.error("error", err);
@@ -63,12 +85,17 @@ export default function useShareChartImage(chart: any) {
   };
 
   const copyImageURL = async () => {
-    setActionLoading((prev) => ({ ...prev, copyImage:true }));
+    setActionLoading((prev) => ({ ...prev, copyImage: true }));
     try {
-      const callback = await startSession();
-      const redirectURI = `${callback.redirect_url}?t=${Date.now()}?point=${STARTING_POINT}`;
-      if (redirectURI) {
+      const imageData = await createWaterMark();
+      if (imageData) {
+        const callback = await startSession(imageData);
+        const redirectURI = `${
+          callback.redirect_url
+        }?t=${Date.now()}?point=${STARTING_POINT}`;
+        if (redirectURI) {
           await navigator.clipboard.writeText(redirectURI);
+        }
       }
     } catch (err) {
       console.error("error", err);
@@ -77,5 +104,5 @@ export default function useShareChartImage(chart: any) {
     }
   };
 
-  return { shareOnTwitter, shareOnTelegram, actionLoading, copyImageURL};
+  return { shareOnTwitter, shareOnTelegram, actionLoading, copyImageURL };
 }
