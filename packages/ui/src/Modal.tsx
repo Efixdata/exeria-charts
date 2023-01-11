f2c740b5c616b4bde19a6a5f6b025b76dca7d776
Kuba Korecki <kubakorecki@gmail.com>
2023-01-11 10:22:44 +0100

    change top, left, right, bottom to inset

--- packages/ui/src/Modal.tsx
+++ packages/ui/src/Modal.tsx
@@ -19,10 +19,7 @@ const Container = styled.div`
 `;
   
 const BackDrop = styled.div`
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+    inset: 0;
     display: flex;
     backdrop-filter: blur(5px);
     z-index: -1;
