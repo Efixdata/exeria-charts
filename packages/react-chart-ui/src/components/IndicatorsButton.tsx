5195e68eec6e17d298d61f1fb2e6c95866fc465a
Kuba Korecki <kubakorecki@gmail.com>
2023-02-07 17:05:14 +0100

    fix indicators dialog on fullscreen CU-862j3gcj0

--- packages/react-chart-ui/src/components/IndicatorsButton.tsx
+++ packages/react-chart-ui/src/components/IndicatorsButton.tsx
@@ -60,12 +60,23 @@ export const IndicatorsButton = (props) => {
     setModalVisible(false);
   };
 
+  const getPortalNode = () => {
+    if (!document) return null;
+
+    if (document.fullscreenElement) return document.fullscreenElement;
+    if (document.webkitFullscreenElement) return document.webkitFullscreenElement;
+    if (document.mozFullScreenElement) return document.mozFullScreenElement;
+    if (document.msFullscreenElement) return document.msFullscreenElement;
+
+    return null;
+  };
+
   return (
     <>
       <TextButton onClick={onClick}>
         <Indicators/> <IndicatorsText>Indicators</IndicatorsText>
       </TextButton>
-      <Portal>
+      <Portal node={getPortalNode()}>
         <Modal
           visible={isModalVisible}
           onCloseOutsideClick={true}
