028d416ead659dad21566871347f4cf06919976e
Kuba Korecki <kubakorecki@gmail.com>
2023-06-19 14:48:14 +0200

    chore: remove self from objectsManager

--- packages/chart/src/ObjectsManager.js
+++ packages/chart/src/ObjectsManager.js
@@ -5,11 +5,10 @@ import { Shape } from "./Objects2";
 
 export default function ObjectsManager(chart){
 	this.chart = chart;
-	var self = this;
 	
 	this.moveObjectToPanel = function(o, destPanelId){
-		var sourcePanel = getPanelForObject(o);
-		var destPanel = getPanelById(destPanelId);
+		var sourcePanel = this.getPanelForObject(o);
+		var destPanel = this.getPanelById(destPanelId);
 		var objects = sourcePanel.objects;
 		for(var j=0; j< objects.length; j++){
 			if(o.id===objects[j].id){
@@ -31,7 +30,7 @@ export default function ObjectsManager(chart){
 	}
 
 	this.cloneObject = function(o){
-		var panel = getPanelForObject(o);
+		var panel = this.getPanelForObject(o);
 		var clone = JSON.parse(JSON.stringify(o));
 		clone.id = FUSION.uniqueId();
 		panel.objects.push(clone);
@@ -39,29 +38,9 @@ export default function ObjectsManager(chart){
 	}
 
 	this.detachObject = function(objectId){
-		if(objectId){
-			var o = LIB.getObjectById(self.chart.model, objectId);
-			if( this.chart.renderer.objects[o.type] instanceof Shape){
-				detachToolObject(o.id);
-				var relatedScript = findRelatedScript(o);
-				if(relatedScript){
-					var plotters = LIB.getPlottersForScriptByScriptId(self.chart.model, relatedScript.id);
-					if(plotters.length > 0)
-						detachSeriesObject(plotters[0]);
-				}
-			}else if( o.type == 'SeriesObject' || o.type=='StrategyObject' || o.type=='CandlestickPatternStrategyObject' || o.type=='FractalsObject'){
-				detachSeriesObject(o);
-			}else{
-				console.log("DELETE: not series object, not tool object....WTF ?", o);
-			}
-			console.log("After delete model:", self.chart.model);
-			console.log("After delete chart.options:", self.chart.options);
-		}
-		
-		
-		function findRelatedScript(o){
-			for(var k in self.chart.model.scripts){
-				var s = self.chart.model.scripts[k];
+		const findRelatedScript = (o) => {
+			for(var k in this.chart.model.scripts){
+				var s = this.chart.model.scripts[k];
 				if(s.inputs){
 					for(let i in s.inputs){
 						if(s.inputs[i].id == o.id)
@@ -70,27 +49,47 @@ export default function ObjectsManager(chart){
 				}
 			}
 			return null;
+		};
+
+		if(objectId){
+			var o = LIB.getObjectById(this.chart.model, objectId);
+			if( this.chart.renderer.objects[o.type] instanceof Shape){
+				this.detachToolObject(o.id);
+				var relatedScript = findRelatedScript(o);
+				if(relatedScript){
+					var plotters = LIB.getPlottersForScriptByScriptId(this.chart.model, relatedScript.id);
+					if(plotters.length > 0)
+					this.detachSeriesObject(plotters[0]);
+				}
+			}else if( o.type == 'SeriesObject' || o.type=='StrategyObject' || o.type=='CandlestickPatternStrategyObject' || o.type=='FractalsObject'){
+				this.detachSeriesObject(o);
+			}else{
+				console.log("DELETE: not series object, not tool object....WTF ?", o);
+			}
+			console.log("After delete model:", this.chart.model);
+			console.log("After delete chart.options:", this.chart.options);
 		}
+	
 	}
 	
 	this.detachAllToolObjects = function(){
-		var panels = self.chart.model.panels;
+		var panels = this.chart.model.panels;
 		for(var i =0; i< panels.length; i++){
 			for(var j =0; j< panels[i].objects.length; j++){
 				var o = panels[i].objects[j];
 				if( this.chart.renderer.objects[o.type] instanceof Shape){
-					detachToolObject(o.id);
+					this.detachToolObject(o.id);
 					if(j>0) j--;
 				}
 			}
 		}
 		
-		console.log("After delete model:", self.chart.model);
-		console.log("After delete chart.options:", self.chart.options);
+		console.log("After delete model:", this.chart.model);
+		console.log("After delete chart.options:", this.chart.options);
 	}
 	
 	this.detachAllScriptObjects = function(){
-		var panels = self.chart.model.panels;
+		var panels = this.chart.model.panels;
 		for(var i = 0; i < panels.length; i++){
 			for(var j = 0; j < panels[i].objects.length; j++){
 				const object = panels[i].objects[j];
@@ -99,32 +98,32 @@ export default function ObjectsManager(chart){
 					object.type === "CandlestickPatternStrategyObject" ||
 					object.type === "FractalsObject"
 				) {
-					var script = isThisSeriesOutputOfScript(object.dataLink);
+					var script = this.isThisSeriesOutputOfScript(object.dataLink);
 					if(script){
-						detachSeriesObject(object);
+						this.detachSeriesObject(object);
 						if(j > 0) j = 0;
 						if(i > 0) i = 0;
 					}
 				}
 			}
 		}
-		console.log("After delete model:", self.chart.model);
-		console.log("After delete chart.options:", self.chart.options);
+		console.log("After delete model:", this.chart.model);
+		console.log("After delete chart.options:", this.chart.options);
 	}
 	
 	this.detachPanel	= function(panelId){
-		var p = getPanelById(panelId);
+		var p = this.getPanelById(panelId);
 		if(p.main) throw "Can't delete main panel!";
 		
 		while(p.objects.length>0){
 			this.detachObject(p.objects[0].id)
 		}
-		self.removeEmptyPanels();
+		this.removeEmptyPanels();
 	}
 
 	this.detachScript	= function(scriptId){
 		if(scriptId){
-			var s = getScriptControllerById(scriptId)
+			var s = this.getScriptControllerById(scriptId)
 			var resultArray = [];
 			resultArray.push(s);
 			for(var i=0; i< s.length; i++){
@@ -132,43 +131,43 @@ export default function ObjectsManager(chart){
 				for (var property in outputs) {
 					if (outputs.hasOwnProperty(property)) {
 						var dl = outputs[property];
-						getScriptsRelatedToSeries(dl, resultArray);
+						this.getScriptsRelatedToSeries(dl, resultArray);
 					}
 				}
 			}
 			for(var i =resultArray.length-1; i>=0; i--){
-				removeScript(resultArray[i]);
+				this.removeScript(resultArray[i]);
 			}
 		}
 	}
 	
 	this.removeEmptyPanels = function(){
-		var panels = self.chart.model.panels;
+		var panels = this.chart.model.panels;
 		for(var i =0; i< panels.length; i++){
 			var c = panels[i].objects.filter(o => (o.type=="SeriesObject" || o.type=="StrategyObject" || o.type=='CandlestickPatternStrategyObject' || o.type=='FractalsObject')).length;
 			if(c==0){
-				self.chart.removePanelFromModel(panels[i]);
+				this.chart.removePanelFromModel(panels[i]);
 			}
 		}
 	}
 
-	function getSeriesById(objectId){
-		return self.chart.model.objects.seriesManager[objectId]
+	this.getSeriesById = function(objectId) {
+		return this.chart.model.objects.seriesManager[objectId]
 	}
 
-	function getScriptControllerById(scriptId){
-		return self.chart.getScriptsManager()[scriptId];
+	this.getScriptControllerById = function(scriptId){
+		return this.chart.getScriptsManager()[scriptId];
 	}
 
-	function getScriptModelById(scriptId){
-		for(var i=0; i<self.chart.model.scripts.length;i++){
-			if(self.chart.model.scripts[i].id === scriptId)
-				return self.chart.model.scripts[i];
+	this.getScriptModelById = function (scriptId) {
+		for(var i=0; i<this.chart.model.scripts.length;i++){
+			if(this.chart.model.scripts[i].id === scriptId)
+				return this.chart.model.scripts[i];
 		}
 	}
 	
-	function getPanelById(panelId){
-		var panels = self.chart.model.panels;
+	this.getPanelById = function(panelId) {
+		var panels = this.chart.model.panels;
 		for(var i =0; i< panels.length; i++){
 			if(panels[i].id === panelId){
 				return panels[i];
@@ -178,10 +177,10 @@ export default function ObjectsManager(chart){
 	}
 
 
-	function detachToolObject(objectId){
+	this.detachToolObject = function(objectId) {
 		if(objectId){
-			for (var i=0; i<self.chart.model.panels.length; i++) {
-				var objects = self.chart.model.panels[i].objects;
+			for (var i=0; i<this.chart.model.panels.length; i++) {
+				var objects = this.chart.model.panels[i].objects;
 				for(var j=0; j< objects.length; j++){
 					if(objectId===objects[j].id){
 						objects.splice(j,1);
@@ -192,54 +191,54 @@ export default function ObjectsManager(chart){
 		}
 	}
 
-	function detachSeriesObject(o){
+	this.detachSeriesObject = function(o) {
 		if(o.dataLink){
 			var result = [];
-			var script = isThisSeriesOutputOfScript(o.dataLink);
+			var script = this.isThisSeriesOutputOfScript(o.dataLink);
 			if(script) result.push(script);
-			getScriptsRelatedToSeries(o.dataLink, result);
+			this.getScriptsRelatedToSeries(o.dataLink, result);
 			console.log("DELETE SERIES: related scripts:", result );
 			for(var i =result.length-1; i>=0; i--){
-				removeScript(result[i]);
+				this.removeScript(result[i]);
 			}
 
-			removeInstrument(o);
+			this.removeInstrument(o);
 		}
 	}
 
-	function removeInstrument(o){
-		if(o.dataLink){
-			var model = self.chart.model;
-			if(o.dataLink == model.mainSeries){
+	this.removeInstrument = function (o) {
+		const removePlotter = (objectId) => {
+			for (var i=0; i<this.chart.model.panels.length; i++) {
+				var objects = this.chart.model.panels[i].objects;
+				for(var j=0; j< objects.length; j++){
+					if(objectId===objects[j].id){
+						objects.splice(j,1);
+						return;
+					}
+				}
+			}
+		}
+
+		if (o.dataLink) {
+			var model = this.chart.model;
+			if (o.dataLink == model.mainSeries) {
 				console.log("Can't delete main series!");				
-			}else{
-				for(var k in self.chart.model.instrumentsSeries){
-					if(self.chart.model.instrumentsSeries[k].seriesId == o.dataLink){
-						self.chart.onInstrumentRemoved(self.chart.model.instrumentsSeries[k].instrument.id);
-						self.chart.model.instrumentsSeries.splice(k,1);
+			} else {
+				for(var k in this.chart.model.instrumentsSeries){
+					if(this.chart.model.instrumentsSeries[k].seriesId == o.dataLink){
+						this.chart.onInstrumentRemoved(this.chart.model.instrumentsSeries[k].instrument.id);
+						this.chart.model.instrumentsSeries.splice(k,1);
 						break;
 					}
 				}
-				delete self.chart.getSeriesManager()[o.dataLink];
+				delete this.chart.getSeriesManager()[o.dataLink];
 				removePlotter(o.id);
-			}
-		}
-
-		function removePlotter(objectId){
-			for (var i=0; i<self.chart.model.panels.length; i++) {
-				var objects = self.chart.model.panels[i].objects;
-				for(var j=0; j< objects.length; j++){
-					if(objectId===objects[j].id){
-						objects.splice(j,1);
-						return;
-					}
-				}
 			}
 		}
 	}
 
-	function removeScript(s){
-		var model = self.chart.model;
+	this.removeScript = function (s) {
+		var model = this.chart.model;
 
 		//model script objects
 		for(var i=0; i<model.scripts.length;i++){
@@ -269,7 +268,7 @@ export default function ObjectsManager(chart){
 				}
 
 				//series
-				delete self.chart.getSeriesManager()[dl];
+				delete this.chart.getSeriesManager()[dl];
 			}
 		}
 		
@@ -282,28 +281,28 @@ export default function ObjectsManager(chart){
 		}
 
 		//delete script controller
-		delete self.chart.getScriptsManager()[s.id];
+		delete this.chart.getScriptsManager()[s.id];
 
-		self.removeEmptyPanels();
+		this.removeEmptyPanels();
 	}
 
 
-	function getScriptsRelatedToSeries(dataLink, resultArray){
-		var s = isThisSeriesInputOfScript(dataLink);
+	this.getScriptsRelatedToSeries = function(dataLink, resultArray){
+		var s = this.isThisSeriesInputOfScript(dataLink);
 		resultArray.push.apply(resultArray,s);
 		for(var i=0; i< s.length; i++){
 			var outputs = s[i].outputs;
 			for (var property in outputs) {
 				if (outputs.hasOwnProperty(property)) {
 					var dl = outputs[property];
-					getScriptsRelatedToSeries(dl, resultArray);
+					this.getScriptsRelatedToSeries(dl, resultArray);
 				}
 			}
 		}
 	}
 
-	function isThisSeriesOutputOfScript (dataLink){
-		var sm = self.chart.getScriptsManager()
+	this.isThisSeriesOutputOfScript = function(dataLink){
+		var sm = this.chart.getScriptsManager()
 		//iterate over scripts
 		for (var property in sm) {
 			if (sm.hasOwnProperty(property)) {
@@ -321,8 +320,8 @@ export default function ObjectsManager(chart){
 		return null;
 	}
 
-	function isThisSeriesInputOfScript(dataLink){
-		var sm = self.chart.getScriptsManager()
+	this.isThisSeriesInputOfScript = function(dataLink) {
+		var sm = this.chart.getScriptsManager()
 		var result = [];
 		//iterate over scripts
 		for (var property in sm) {
@@ -339,8 +338,8 @@ export default function ObjectsManager(chart){
 		return result;
 	}
 
-	function getPanelForObject(o){
-		var panels = self.chart.model.panels;
+	this.getPanelForObject = function(o) {
+		var panels = this.chart.model.panels;
 		for(var i =0; i< panels.length; i++){
 			for(var j =0; j< panels[i].objects.length;j++){
 				if(panels[i].objects[j].id === o.id)
